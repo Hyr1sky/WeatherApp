9c3db20d1497fd56997ad8f12a9a08b5d2900c2c
Hyr1sky <hyrisky272@outlook.com>
2024-01-16 15:04:46 +0800

    Hands-on project

---
+++ vue-project/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#62E7AD",
+        "weather-secondary": "#1AC379",
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
+    },
+  },
+  plugins: [],
+}
