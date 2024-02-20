2e5e5945b507bbc1ac048c016dda69f5cccb37bf
Hyr1sky <hyrisky272@outlook.com>
2024-02-20 11:43:47 +0800

    God will save my ass with a VISA card

---
+++ vue-project/src/views/AsyncCityView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col flex-1 items-center">
+    <!-- Banner -->
+    <div
+      v-if="route.query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center"
+    >
+      <p>
+        You are currently previewing this city, click "+" icon to start tracking
+        this city.
+      </p>
+    </div>
+    <!-- Weather Overview -->
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=eb656c82a355328f7439b3738e0576c4`
+    );
+
+    // cal current data & time
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.current.dt * 1000 + localOffset;
+    weatherData.data.currentTime =
+      utc + 1000 * weatherData.data.timezone_offset;
+
+    // cal hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+    });
+
+    return weatherData;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -13,13 +13,30 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="amapSearchResults"
       >
-        <li
-          v-for="searchResult in amapSearchResults"
-          :key="searchResult.id"
-          class="py-2 px-2 hover:bg-weather-primary cursor-pointer"
-        >
-          {{ searchResult.status }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!searchError && amapSearchResults.lives.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <div
+            class="py-2 px-2 hover:bg-weather-primary cursor-pointer"
+            @click="previewCity(amapSearchResults)"
+          >
+            {{ amapSearchResults.lives[0].province }}
+            {{ amapSearchResults.lives[0].city }}
+          </div>
+        </template>
+        <!-- 
+        <template v-else>
+          <li
+            v-for="searchResult in amapSearchResults"
+            :key="searchResult.id"
+            class="py-2 px-2 hover:bg-weather-primary cursor-pointer"
+          >
+            {{ searchResult.status }}
+          </li>
+        </template>
+         -->
       </ul>
     </div>
   </main>
@@ -28,21 +45,48 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previewCity = (amapSearchResults) => {
+  console.log(amapSearchResults);
+  const [province, city] = [
+    amapSearchResults.lives[0].province,
+    amapSearchResults.lives[0].city,
+  ];
+  const cityCode = amapSearchResults.lives[0].adcode;
+  router.push({
+    name: "cityView",
+    params: { state: province, city: city },
+    // params: { cityCode: cityCode },
+    query: {
+      // lat: 
+      // lon: 
+      preview: true,
+    }
+  });
+};
 
 const amapAPIKey = "254c5f491cf8d907054744313d06d9b8";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const amapSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value != "") {
-      const result = await axios.get(
-        `https://restapi.amap.com/v3/weather/weatherInfo?city=${searchQuery.value}&key=${amapAPIKey}&type=place`
-      );
-      amapSearchResults.value = result.data;
-      console.log(amapSearchResults.value);
+      try {
+        const result = await axios.get(
+          `https://restapi.amap.com/v3/weather/weatherInfo?city=${searchQuery.value}&key=${amapAPIKey}&type=place`
+        );
+        amapSearchResults.value = result.data;
+        // console.log(amapSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
+
       return;
     }
     amapSearchResults.value = null;
